6dc245df78d9e67d1875090ecc6f0986ce1f4b00
scug <scug@VXN-MacPro-3.local>
2019-12-08 16:13:35 -0600

    js

---
+++ decoratemytext.js
@@ -0,0 +1,51 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+//document.getElementById("btn").addEventListener("click", function(){
+// alert("Hello World");
+// });
+window.onload = function () {
+    document.getElementById("txt").value = "";
+    
+};
+
+var fontSize =  parseInt(getComputedStyle(document.getElementById("txt")).getPropertyValue('font-size')); 
+var f1 = 0;
+document.getElementById("btn").onclick = function () {  
+    delayMsg(fontSize);    
+    if(document.getElementById("checked").checked === true){
+        document.getElementById("txt").style.fontWeight = "bold";
+        document.getElementById("txt").style.color = "green";
+        document.getElementById("txt").style.textDecoration = "underline";        
+        onchange();
+    }else if(document.getElementById("checked").checked === false){ 
+        document.getElementById("txt").style.fontWeight = "normal";        
+        document.getElementById("txt").style.color = null;
+        document.getElementById("txt").style.textDecoration = null;                
+        onchange();        
+    }
+};
+
+function onchange(){
+    alert("Bling has been added!!!");
+}
+
+var timer = null;
+function delayMsg(fontSize) {
+  if (timer === null) {
+    timer = setInterval(increaseFont, 5000, fontSize);
+  } else {
+    clearInterval(timer);
+    timer = null;
+  }
+}
+function increaseFont(fontSize) {
+   f1 += fontSize+ 2;
+  document.getElementById("txt").style.fontSize = f1 +"pt";
+}
+
+
